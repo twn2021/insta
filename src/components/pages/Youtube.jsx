778fb768d3219a8767920cc54ea307df34a9d515
twn2021 <sagamk2010@gmail.com>
2022-01-10 19:15:42 +0900

    유뷰투
    유튜부 메인페이지

--- src/components/pages/Youtube.jsx
+++ src/components/pages/Youtube.jsx
@@ -3,6 +3,7 @@ import ModalSidebar from "../youtube/ModalSidebar";
 import Topbar from "../youtube/Topbar";
 import videoList from "../../data/video";
 import VideoList from "../youtube/VideoList";
+import styled from "styled-components";
 
 const Youtube = () => {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -15,9 +16,17 @@ const Youtube = () => {
       {/* <button onClick={() => setShowSidebar(true)}>sidebar</button> */}
       {showSidebar && <ModalSidebar onClose={hideSidebar} />}
       {/* 클릭시 생성 제거 */}
-      <VideoList data = {videoList} />
+
+      <Wrapper>
+        <VideoList data = {videoList} />
+      </Wrapper>
     </div>
   );
 };
+
+const Wrapper = styled.div`
+display: flex;
+flex-wrap: wrap;
+`;
 
 export default Youtube;
--- src/components/youtube/VideoList.jsx
+++ src/components/youtube/VideoList.jsx
@@ -5,33 +5,86 @@ const VideoList = ({data}) => {
 
     return (
         <>
+        <Wrap>
         <Container>
             {data.map(({id,title,view,timestamp,channelImage,channel,image,}) => (
             <Short key={id}>
-                <Title src={title}/>
-                <View src={view}/>
-                <Timestamp src={timestamp}/>
-                <ChannelImage src={channelImage}/>
-                <Channel src={channel}/>
-                <Image src={image}/>
+                    <ChannelImage src={channelImage}/>
+                <Info>
+
+                    <Avatar >
+                        <Channel src={channel} />
+                        <Image src={image} />
+
+                       
+                    </Avatar>
+
+                    <Text >
+                        <Title src={title}/>
+                        <View src={view}/>
+                        <Timestamp src={timestamp}/>
+                    </Text> 
+                        
+                    
+                </Info>
             </Short>
             ))}
         </Container>
+        </Wrap>
         </>
       
     )
-};
+}; 
+
 
 const Container = styled.div`
-width: 400px;
-height: 500px;
+display: flex;
+flex-wrap: wrap;
 `;
-const Short = styled.div``;
+
+
+const Wrap = styled.div`
+background-color: #f9f9f9;
+padding: 40px 20px;
+padding-bottom: 0;
+`;
+
+
+const Info = styled.div`
+display: flex;
+margin-top: 10px;
+padding-right: 30px;
+`;
+
+
+
+const Avatar = styled.div`
+`;
+
+
+const Text = styled.p`
+margin-left: 15px;
+`;
+
+
+
+const Short = styled.div`
+margin-bottom: 40px;
+width: 270px;
+`;
 const Title = styled.div``;
 const View = styled.div``;
 const Timestamp= styled.div``;
-const ChannelImage = styled.div``;
-const Channel = styled.div``;
-const Image = styled.div``;
+
+
+const ChannelImage = styled.img`
+height: 140px;
+width: 250px;
+`;
+const Channel = styled.p``;
+
+
+const Image = styled.img`
+`;
 
 export default VideoList; 
