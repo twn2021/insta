af206eaa76bb0c243355b8d7d16f1a8b7b772181
twn2021 <sagamk2010@gmail.com>
2022-01-24 21:14:25 +0900

    landing page update
    렌딩 페이지 다듬기

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -3,17 +3,27 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>
+  
+    <Wrap>
       <div>
-        <p>안녕</p>
-        <p>이건 리엑트로 만들었지.</p>
+        <p>REACT로 만든 인스타그램 입니다.</p>
       </div>
-      <Link to="/Login">
-      <EnterBtn>출발</EnterBtn>
-      </Link>
-    </div>
+        <Link to="/Login">
+        <EnterBtn>출발</EnterBtn>
+        </Link>
+    </Wrap>
   );
+
 };
+
+const Wrap = styled.div`
+font-size: 20px;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 100px;
+`;
+
 
 const EnterBtn = styled.button`
 background-color: #e5ff00;
